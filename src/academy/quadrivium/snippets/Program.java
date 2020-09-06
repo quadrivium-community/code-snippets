6841de11b77aad0daf2065bbb341c27f6e024e12
olegzavialov <oleg.zavialov.ee@gmail.com>
2020-09-06 18:34:13 +0300

    Add program entry point class Program

---
+++ src/academy/quadrivium/snippets/Program.java
@@ -0,0 +1,17 @@
+package academy.quadrivium.snippets;
+
+public class Program {
+
+    public static void main(String[] arguments) {
+        System.out.println(exp(11));
+    }
+
+    // Example static method
+    public static int exp(int b) {
+        return b * b;
+    }
+
+}
+
+
+
