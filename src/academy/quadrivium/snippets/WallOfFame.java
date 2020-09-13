ff8177d357f748f709525a05e4d7c41aadf1ea1a
emote1 <valera14.92@mail.ru>
2020-09-13 14:11:49 +0300

    ValerkaProject

---
+++ src/academy/quadrivium/snippets/WallOfFame.java
@@ -0,0 +1,18 @@
+package academy.quadrivium.snippets;
+
+import java.time.Year;
+
+public class WallOfFame {
+   private String Bondora = "Кредит";
+   private int Month = 12;
+   private int Size = 1000;
+   private Object Year;
+
+
+    public void WallOfFame (){
+
+   Year = Month * Size;
+   return;
+
+}
+}
