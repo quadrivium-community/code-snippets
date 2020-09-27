0f1a6af77d06ce6ffa2969d00ac1794cffd3985d
Yegor Karimov <egor.karimov@gmail.com>
2020-09-27 17:24:54 +0300

    Add sorting method

--- src/academy/quadrivium/snippets/SortingProgram.java
+++ src/academy/quadrivium/snippets/SortingProgram.java
@@ -2,10 +2,24 @@ package academy.quadrivium.snippets;
 
 public class SortingProgram {
 
-    public static void main(String[] arguments) {
+	public static void main(String[] arguments) {
+		// array yo sort
+		final char[] chars = {'a', 'b', 'b', 'a', 'a', 'b', 'b', 'c', 'a', 'd', 'a', 'b', 'b', 'a', 'a', 'b', 'b', 'a'};
+		System.out.println("\n\n==============S=O=R=T=I=N=G==============");
 
-    }
+		System.out.print("Array of not sorted characters: ");
+		System.out.println(chars);
 
+		final char[] sortedArray = sortAscending(chars);
+
+		System.out.print("Sorted array of characters: ");
+		System.out.println(sortedArray);
+	}
+
+	private static char[] sortAscending(char[] arrayToSort) {
+		// implement sorting here
+		return new char[] {}; // return correct sorted array
+	}
 }
 
 
--- src/academy/quadrivium/snippets/type/StringImplementation.java
+++ src/academy/quadrivium/snippets/type/StringImplementation.java
@@ -2,4 +2,28 @@ package academy.quadrivium.snippets.type;
 
 public class StringImplementation {
 
+	private char[] container = new char[10];
+
+	public StringImplementation(char[] chars) {
+		if (chars.length != 10) {
+			System.out.println("Sorry, please use exactly 10 characters.");
+			return;
+		}
+
+		container[0] = chars[0];
+		container[1] = chars[1];
+		container[2] = chars[2];
+		container[3] = chars[3];
+		container[4] = chars[4];
+		container[5] = chars[5];
+		container[6] = chars[6];
+		container[7] = chars[7];
+		container[8] = chars[8];
+		container[9] = chars[9];
+	}
+
+	public void printString() {
+		System.out.println(container);
+	}
+
 }
