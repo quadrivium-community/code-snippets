79ac02e993b9efceaeb5e51017dca451f8759964
Vladimir <vlad.vander@gmail.com>
2020-10-05 23:53:44 +0300

    Bubble Sorting Vlad Char.

---
+++ src/academy/quadrivium/snippets/sorter/ArrayBubbleSortVladChar.java
@@ -0,0 +1,32 @@
+package academy.quadrivium.snippets.sorter;
+
+
+public class ArrayBubbleSortVladChar {
+    public static void main(String[] args) {
+
+        char[] A = {'a','f','r','t','1','7','%','$'};
+        boolean swapped = true;
+        while(swapped){
+            swapped=swap(A);
+        }
+
+        for (char element: A) {
+            System.out.println(element);
+        }
+    }
+
+    public static boolean swap(char[] A){
+        boolean swapped=false;
+        for (int i = 0; i < A.length - 1; i++){
+            if(A[i] > A[i+1]){
+                char temp;
+                temp = A[i];
+                A[i] = A[i+1];
+                A[i+1] = temp;
+                swapped=true;
+            }
+        }
+        return swapped;
+    }
+
+}
