5015bf465112146911367d2e1bca8ecf5c1b576a
emote <valera14.92@mail.ru>
2020-10-06 21:23:50 +0300

    SortingChars

--- src/academy/quadrivium/snippets/sorter/SortingInteger.java
+++ src/academy/quadrivium/snippets/sorter/SortingInteger.java
@@ -4,22 +4,21 @@ import java.util.Arrays;
 
 public class SortingInteger {
     public static void main(String[] arguments) {
-        int[] papaya = {1, 6, 8, 9, 10, 4, 2};
+        char[] papaya = {'f','t','T','e','#','T', 'B','a'};
         boolean swapped = true;
 
         while (swapped) {
             swapped = swap(papaya);
         }
 
-        Arrays.stream(papaya).forEach(System.out::println);
-
+        System.out.println(papaya);
     }
 
-    private static boolean swap(int[] array) {
+    private static boolean swap(char[] array) {
         boolean count = false;
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) {
-                int temp;
+                char temp;
                 temp = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = temp;
@@ -28,5 +27,4 @@ public class SortingInteger {
         }
         return count;
     }
-
 }
