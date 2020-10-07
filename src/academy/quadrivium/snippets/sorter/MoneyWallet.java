cdb95e9cd35d28ae08d447125bfa6ae2b1afb9e7
Aleksei Tulin <lehaxe@yahoo.com>
2020-10-07 23:10:30 +0300

    Update sorter
    * Now the console shows good information
    * Now we can know how many successful and unsuccessful changes were in the array.
    * Optimizing the swap function

--- src/academy/quadrivium/snippets/sorter/MoneyWallet.java
+++ src/academy/quadrivium/snippets/sorter/MoneyWallet.java
@@ -1,31 +1,42 @@
 package academy.quadrivium.snippets.sorter;
-
 import java.util.Arrays;
 
 public class MoneyWallet {
 
+    // Number of changes in the array
+    private static int countChangesInArray;
+    private static int countNoChangesInArray;
+    private static final int[] moneyWallet = {200, 2040, 29283, 39, 921, 10, 22, 0, 1, 99};
+
     public static void main(String[] arguments) {
         boolean swapped = true;
-        int[] moneyWallet = {200, 2040, 29283, 39, 921, 10, 22, 0, 1, 99};
+        System.out.println("\nНе отсортированный массив:" + Arrays.toString(moneyWallet));
 
         while (swapped) {
             swapped = swap(moneyWallet);
         }
 
-        Arrays.stream(moneyWallet).forEach(System.out::println);
+        System.out.println(
+                "Результат отсортированного массива:" + Arrays.toString(moneyWallet) +
+                "\nМетод swap произвел:" + countChangesInArray + " изменений/я в массиве " +
+                "\nМетод swap ничего " + "не изменил в массиве:" + countNoChangesInArray + " раз/а"
+        );
     }
+
     public static boolean swap(int[] array){
-        boolean count = false;
+        boolean haveTheValuesChanged = false;
 
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) {
-                int temp;
-                temp = array[i];
+                int temp = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = temp;
-                count = true;
+                haveTheValuesChanged = true;
+                countChangesInArray ++;
+            } else {
+                countNoChangesInArray ++;
             }
         }
-        return count;
+        return haveTheValuesChanged;
     }
 }
