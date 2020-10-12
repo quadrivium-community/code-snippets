2ac5f79291c663a17d564efe1e57710be60eddab
Aleksei Tulin <lehaxe@yahoo.com>
2020-10-13 00:35:48 +0300

    Update IntegersSorter.java
    Optimizing the swap method

--- src/academy/quadrivium/snippets/sorter/IntegersSorter.java
+++ src/academy/quadrivium/snippets/sorter/IntegersSorter.java
@@ -20,8 +20,7 @@ public class IntegersSorter {
 		boolean hasSwapped = false;
 		for(int i = 0; i < array.length - 1; i++) {
 			if(array[i] > array[i + 1]) {
-				final int temp;
-				temp = array[i];
+				final int temp = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
 				hasSwapped = true;
