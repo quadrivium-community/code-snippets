2ea08919f0744b45c58930131aaf1d181ed2f218
OlegZavialov <oleg.zavialov.ee@gmail.com>
2020-10-13 19:25:38 +0300

    скопировал готовое

---
+++ src/academy/quadrivium/snippets/sorter/OlegTest.java
@@ -0,0 +1,14 @@
+package academy.quadrivium.snippets.sorter;
+
+public class OlegTest {
+    public class Person {
+        void stop() {
+        }
+        void isLoaded() {
+        }
+        void showDifference() {
+        }
+        public void main(String[] args) {
+        }
+    }
+}
