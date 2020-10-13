742f6b733e6a4019fdfc24bab2501e7474573a44
Aleksei Tulin <lehaxe@yahoo.com>
2020-10-13 20:18:39 +0300

    Refactoring

--- src/academy/quadrivium/snippets/sorter/OlegTest.java
+++ src/academy/quadrivium/snippets/sorter/OlegTest.java
@@ -1,15 +1,11 @@
 package academy.quadrivium.snippets.sorter;
 
 public class OlegTest {
-    public class Person {
-        void stop() {
-        }
-        public boolean isLoaded() {
-            return true;
-        }
-        void showDifference() {
-        }
-        public void main(String[] args) {
-        }
+    static String isLoaded() {
+        String a = "Привет";
+        return a;
+    }
+    public static void main(String[] args) {
+        System.out.println(OlegTest.isLoaded());
     }
 }
