d7db46df4a660a63f9d054b88f0ba063d1385d03
OlegZavialov <oleg.zavialov.ee@gmail.com>
2020-10-14 20:22:19 +0300

    20-18

--- src/academy/quadrivium/snippets/sorter/OlegTest.java
+++ src/academy/quadrivium/snippets/sorter/OlegTest.java
@@ -2,29 +2,23 @@ package academy.quadrivium.snippets.sorter;
 
 // класс OlegTest
 public class OlegTest {
-
     // Первый метод stop ничего не возвращает и ничего не принимает
     void stop() {
     }
-
     // Второй метод isLoaded возвращает значение типа boolean и ничего не принимает.
 static boolean isLoaded() {
         return true;
     }
-
     //Третий метод showDifference ничего не возвращает и принимает два массива с типом и названием на твоё усмотрение.
 
-
-
     // метод main
+    // Обращаемся к методу isLoaded класса OlegTest
+    // и выводим его значение в консоль. (в консоли получаем значение true)
     public static void main(String[] args) {
         System.out.println(OlegTest.isLoaded());
     }
     public static void showDifference(String[] stroka, int[] args) {
 
-
-        // Обращаемся к методу isLoaded класса OlegTest
-        // и выводим его значение в консоль. (в консоли получаем значение true)
 
     }
 }
