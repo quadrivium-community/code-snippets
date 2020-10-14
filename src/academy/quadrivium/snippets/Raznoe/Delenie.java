78b935a0b12ea52e2ed1da214e5f3cf59edca883
OlegZavialov <oleg.zavialov.ee@gmail.com>
2020-10-14 21:28:57 +0300

    Вуаля

--- src/academy/quadrivium/snippets/Raznoe/Delenie.java
+++ src/academy/quadrivium/snippets/Raznoe/Delenie.java
@@ -2,7 +2,7 @@ package academy.quadrivium.snippets.Raznoe;
 // Необходимо вывести остаток от деления первого числа на второе
 public class Delenie {
             public static void main(String[] args) {
-                long a = 55656567464;
+                long a = 55656567464L;
                 long b = 674764;
                 System.out.println(a%b);
         }
