5e00ea0c9a435a83be3eed0786958408f92b3c61
OlegZavialov <oleg.zavialov.ee@gmail.com>
2020-10-14 21:06:27 +0300

    Вуаля

---
+++ src/academy/quadrivium/snippets/Raznoe/Delenie.java
@@ -0,0 +1,9 @@
+package academy.quadrivium.snippets.Raznoe;
+
+public class Delenie {
+            public static void main(String[] args){
+            long a = 55656567464;
+            long b = 674764;
+            System.out.println(a%b);
+        }
+    }
