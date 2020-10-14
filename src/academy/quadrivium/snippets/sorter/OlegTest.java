334d61695c9cd68100132bb14003ea9378d2c254
OlegZavialov <oleg.zavialov.ee@gmail.com>
2020-10-14 20:26:55 +0300

    20-18

--- src/academy/quadrivium/snippets/sorter/OlegTest.java
+++ src/academy/quadrivium/snippets/sorter/OlegTest.java
@@ -12,7 +12,6 @@ static boolean isLoaded() {
     //Третий метод showDifference ничего не возвращает и принимает два массива с типом и названием на твоё усмотрение.
 
     // метод main
-
     public static void main(String[] args) {
         // Обращаемся к методу isLoaded класса OlegTest
         // и выводим его значение в консоль. (в консоли получаем значение true)
