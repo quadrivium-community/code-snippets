6fc398d3517709904691805ed124a5796f0b8ebe
Aleksei Tulin <lehaxe@yahoo.com>
2020-10-14 20:33:28 +0300

    Refactoring

--- src/academy/quadrivium/snippets/sorter/OlegTest.java
+++ src/academy/quadrivium/snippets/sorter/OlegTest.java
@@ -2,16 +2,18 @@ package academy.quadrivium.snippets.sorter;
 
 // класс OlegTest
 public class OlegTest {
+
     // Первый метод stop ничего не возвращает и ничего не принимает
     void stop() {
     }
 
     // Второй метод isLoaded возвращает значение типа boolean и ничего не принимает.
-
-static boolean isLoaded() {
+    static boolean isLoaded() {
         return true;
     }
+
     //Третий метод showDifference ничего не возвращает и принимает два массива с типом и названием на твоё усмотрение.
+    -> тут напиши третий метод по заданию <-
 
     // метод main
     public static void main(String[] args) {
@@ -19,8 +21,8 @@ static boolean isLoaded() {
         // и выводим его значение в консоль. (в консоли получаем значение true)
         System.out.println(OlegTest.isLoaded());
     }
-    public static void showDifference(String[] stroka, int[] args) {
 
+    public static void showDifference(String[] stroka, int[] args) {
 
     }
 }
