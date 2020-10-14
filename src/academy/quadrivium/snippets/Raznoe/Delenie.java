cdc57504af16f1b1ca496e1e8c2d3e1b82abe8cc
OlegZavialov <oleg.zavialov.ee@gmail.com>
2020-10-14 21:08:38 +0300

    Вуаля

--- src/academy/quadrivium/snippets/Raznoe/Delenie.java
+++ src/academy/quadrivium/snippets/Raznoe/Delenie.java
@@ -4,6 +4,7 @@ public class Delenie {
             public static void main(String[] args){
             long a = 55656567464;
             long b = 674764;
-            System.out.println(a%b);
+            long c = a/b;
+            System.out.println(c);
         }
     }
