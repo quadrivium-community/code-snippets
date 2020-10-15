8729101e7e29be1ba2cbca4e9e82696f5eb17dc2
OlegZavialov <oleg.zavialov.ee@gmail.com>
2020-10-15 19:01:06 +0300

    Вуаля

---
+++ src/academy/quadrivium/snippets/Raznoe/peremennieformatvivoda.java
@@ -0,0 +1,14 @@
+package academy.quadrivium.snippets.Raznoe;
+
+import java.util.Scanner;
+
+public class peremennieformatvivoda {
+           public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int a,b;
+            a = in.nextInt();         //  оператор ввода для переменной a
+            b = in.nextInt();         //  оператор ввода для переменной b
+            System.out.printf("%d %d \n", a, b);         // оператор вывода значений всех переменных на экран в алфавитном порядке через пробел
+
+        }}
+
