ebce2521db04b4de13b03d7eede05a0988b05c29
OlegZavialov <oleg.zavialov.ee@gmail.com>
2020-10-15 19:37:41 +0300

    Вуаля

---
+++ src/academy/quadrivium/snippets/Raznoe/chirinapolja.java
@@ -0,0 +1,10 @@
+package academy.quadrivium.snippets.Raznoe;
+
+public class chirinapolja {
+         public static void main(String[] args) {
+           int a = 10;
+           System.out.printf("%010d",a);       // 0000000010
+
+        }
+}
+
