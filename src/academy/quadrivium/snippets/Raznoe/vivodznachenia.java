e416baa6ab11ada8b776fdbaed055ce7ca76dab4
OlegZavialov <oleg.zavialov.ee@gmail.com>
2020-10-16 12:49:44 +0300

    Вуаля

---
+++ src/academy/quadrivium/snippets/Raznoe/vivodznachenia.java
@@ -0,0 +1,11 @@
+package academy.quadrivium.snippets.Raznoe;
+
+import java.util.Locale;
+
+public class vivodznachenia {
+            public static void main(String[] args) {
+            double a = 543.42375;
+            System.out.printf("%+.3f", a);        // вывод значения переменной a cо знаком и с точностью 3 знака после запятой
+
+        }
+}
