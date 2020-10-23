56c0499cfddc576ddc84538787ff51b43ce04531
OlegZavialov <oleg.zavialov.ee@gmail.com>
2020-10-23 23:45:23 +0300

    Теперь правильно

--- src/academy/quadrivium/snippets/Raznoe/kvadrat.java
+++ src/academy/quadrivium/snippets/Raznoe/kvadrat.java
@@ -1,10 +1,10 @@
 package academy.quadrivium.snippets.Raznoe;
 
-public class kvadrat {
+class kvadrat {
     public static void main(String[] args) {
-        double x = 1;
-        double y = 1 - (Math.sqrt(x) + 2.5 * (x * Math.sqrt(x)) + Math.sqrt(x) *Math.sqrt(x));
-        double z = y*y;
+        double x=1;
+        double y=(1-Math.pow(x,2))+2.5*(Math.pow(x,3))+Math.pow(x,4);
+        double z=y*y;
         System.out.print(z);
     }
 }
