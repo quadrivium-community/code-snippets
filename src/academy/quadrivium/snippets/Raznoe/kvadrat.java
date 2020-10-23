df0b991dffdcc8af62c981fe7f3b25be9eca34c6
OlegZavialov <oleg.zavialov.ee@gmail.com>
2020-10-23 20:19:44 +0300

    Частично правильно при х=1 При х=2 неправильно

--- src/academy/quadrivium/snippets/Raznoe/kvadrat.java
+++ src/academy/quadrivium/snippets/Raznoe/kvadrat.java
@@ -1,12 +1,10 @@
 package academy.quadrivium.snippets.Raznoe;
 
-import java.util.Scanner;
-
 public class kvadrat {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        double x = 0;
-        double y = ((1 - x * x) + (2.5 * x * x * x) + (x * x * x * x)*(1 - x * x) + (2.5 * x * x * x) + (x * x * x * x));
-        System.out.print(y);
+        double x = 1;
+        double y = 1 - (Math.sqrt(x) + 2.5 * (x * Math.sqrt(x)) + Math.sqrt(x) *Math.sqrt(x));
+        double z = y*y;
+        System.out.print(z);
     }
 }
