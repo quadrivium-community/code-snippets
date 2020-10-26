d10e20fe9fa34d8395669331b74a6e3308d006e1
Aleksei Tulin <lehaxe@yahoo.com>
2020-10-26 11:26:51 +0200

    Refactoring

--- src/academy/quadrivium/snippets/AlekseiTulin/Calculator.java
+++ src/academy/quadrivium/snippets/AlekseiTulin/Calculator.java
@@ -1,16 +1,20 @@
 package academy.quadrivium.snippets.AlekseiTulin;
-import java.util.*;
 
+import java.util.*;
 public class Calculator {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        double result;
+        String message[] = {"\nInsert first number, operator (+, -, *, /, %) and second number separated by a \" +\n" +
+                "\"space or new line, and press Enter: \n", "\\n\\n========================\" +\n" +
+                "\"\\nError! \\nOperator is not correct, please press 'Return 'Calculator'' of that \" +\n" +
+                "\"application and try again.\\n========================\\n"
+        };
 
-        System.out.print("\nInsert first number, operator (+, -, *, /, %) and second number separated by a " +
-                "space or new line, and press Enter: ");
+        System.out.print(message[0]);
+        Scanner in = new Scanner(System.in);
         Double first = in.nextDouble();
         char operator = in.next().charAt(0);
         Double second = in.nextDouble();
+        Double result;
 
         switch(operator) {
             case '*':
@@ -29,9 +33,7 @@ public class Calculator {
                 result = first % first;
                 break;
             default:
-                throw new RuntimeException("\n\n========================" +
-                        "\nError! \nOperator is not correct, please press 'Return 'Calculator'' of that " +
-                        "application and try again.\n========================\n");
+                throw new RuntimeException(message[1]);
         }
 
         System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
