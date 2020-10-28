1c6514b928a9302cf98ca81dd31be1f2904da42d
Aleksei Tulin <lehaxe@yahoo.com>
2020-10-28 23:30:26 +0200

    Refactoring: Loop and CatchError
    * Loop calc if result.value == "null"
    * Catch error "InputMismatchException"

--- src/academy/quadrivium/snippets/AlekseiTulin/Calculator.java
+++ src/academy/quadrivium/snippets/AlekseiTulin/Calculator.java
@@ -11,31 +11,43 @@ public class Calculator implements InterfaceForApplication {
 
         System.out.print("Insert your name: ");
         System.out.print("\nHello " + name);
-        System.out.print("\nHow work calculator: Insert first number, operator (+, -, *, /, %) and second number separated by a space or new line, and press Enter: \n");
+        System.out.print("\nHow work calculator: Insert first number, operator (+, -, *, /, %) and second number " +
+                "separated by a space or new line, and press Enter: \n");
 
-        Double first = in.nextDouble();
-        char operator = in.next().charAt(0);
-        Double second = in.nextDouble();
         Double result = null;
 
-        switch(operator) {
-            case '*':
-                result = first * second;
-                break;
-            case '/':
-                result = first / second;
-                break;
-            case '+':
-                result = first + second;
-                break;
-            case '-':
-                result = first - second;
-                break;
-            case '%':
-                result = first % second;
-            default:
-                System.out.print("Operator is not correct, please insert first number, operator and second number again, and press Enter.");
+        try {
+            while(result == null) {
+                Double first = in.nextDouble();
+                char operator = in.next().charAt(0);
+                Double second = in.nextDouble();
+
+                switch(operator) {
+                    case '*':
+                        result = first * second;
+                        break;
+                    case '/':
+                        result = first / second;
+                        break;
+                    case '+':
+                        result = first + second;
+                        break;
+                    case '-':
+                        result = first - second;
+                        break;
+                    case '%':
+                        result = first % second;
+                    default:
+                        System.out.print("Operator is not correct, please insert first number, operator and second " +
+                                "number again, and press Enter.\n");
+                }
+
+                System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Sorry, claculator not working like that, app stoped, if you want run that app again " +
+                    "please click restartbutton, and try again.");
         }
-        System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
     }
 }
